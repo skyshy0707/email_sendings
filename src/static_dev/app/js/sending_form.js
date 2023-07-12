6bb4ddd3202a9e8e12e4bb3d1ed2c1f52d8622f3
Alexei Mironov <alekszskyshy@gmail.com>
2023-07-12 17:44:47 +0600

    Clean file

--- src/static_dev/app/js/sending_form.js
+++ src/static_dev/app/js/sending_form.js
@@ -1,11 +1,7 @@
-//import { BASE_URL } from '../../common_js/base.js'
 
-console.log("BASE_URL", BASE_URL)
 $("#sendings").on("submit", (event) => {
     event.preventDefault()
-    //data = $(this).seriallize()
     data = new FormData(event.target)
-    console.log("data", data)
     $.ajax({
         type: 'POST',
         data: data,
@@ -14,7 +10,6 @@ $("#sendings").on("submit", (event) => {
         contentType: false,
         url: `${BASE_URL}`,
         beforeSend: function(xhr){
-            //xhr.setRequestHeader('Authorization', 'Token ' + getCookie('access_token'))
             xhr.setRequestHeader('X-CSRF-Token', data.get('csrfmiddlewaretoken'))
         },
         success: function(response, status, jqxhr){
